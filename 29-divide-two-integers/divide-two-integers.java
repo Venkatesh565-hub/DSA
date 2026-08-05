20609e02d398b6d2cb8f79c554694f3969396bee
Venkatesh565-hub <venkeyvangara565@gmail.com>
2026-08-05 16:39:19 +0530

    Time: 1 ms (93.40%) | Memory: 42.6 MB (51.24%) - LeetSync

--- 29-divide-two-integers/divide-two-integers.java
+++ 29-divide-two-integers/divide-two-integers.java
@@ -1,12 +1,10 @@
 class Solution {
     public int divide(int dividend, int divisor) {
 
-        // Handle overflow
         if (dividend == Integer.MIN_VALUE && divisor == -1) {
             return Integer.MAX_VALUE;
         }
 
-        // Determine sign
         boolean negative = (dividend < 0) ^ (divisor < 0);
 
         long a = Math.abs((long) dividend);
